7ea76df00b9b17d06727c36eb60251abd0bea63f
ashwath <ashwathps@users.noreply.github.com>
2013-12-30 14:01:06 +0530

    Update indepth_notes1.scala

--- indepth_notes1.scala
+++ indepth_notes1.scala
@@ -1,4 +1,9 @@
 /*
 Scala in-depth
 
+In Functional approach,  behavioral pieces become arguments to the controlling function
+
+Functional programming is more than combining functions with other functions. The essence of functional programming is delaying side effects as long as possible. This predicate object defines a simple mechanism to combine predicates. The predicate isn’t used to cause side effects until passed to the Iterables object
 */
+
+//FP - focus is on verbs, unlike nouns in OO.
